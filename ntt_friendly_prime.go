7fa414aa2a327bf4a27023fee7ad516d40ce7882
nikola <nikola@30-9-3.wireless.csail.mit.edu>
2022-11-03 13:10:56 -0400

    Auto-generate ntt-friendly primes

--- ntt_friendly_prime.go
+++ ntt_friendly_prime.go
@@ -1,1 +1,18 @@
 package fnttw
+
+func NttFriendlyPrimes(ntt_size, num_primes, bits_per_prime int) []uint64 {
+	primes := make([]uint64, num_primes)
+	if bits_per_prime > 63 {
+		panic("bits_per_prime must be <= 63")
+	}
+	m := (1 << (bits_per_prime - 1)) / ntt_size
+	for i := 0; i < num_primes; i++ {
+		candidate := uint64(0)
+		for !NewBigIntFromUint64(candidate).ProbablyPrime(20) {
+			candidate = uint64(2*ntt_size*m + 1)
+			m += 1
+		}
+		primes[i] = candidate
+	}
+	return primes
+}
--- ntt_friendly_prime_test.go
+++ ntt_friendly_prime_test.go
@@ -1,1 +1,9 @@
 package fnttw
+
+import (
+	"testing"
+)
+
+func TestNttFriendlyPrimes(t *testing.T) {
+	NttFriendlyPrimes(1<<16, 50, 63)
+}
